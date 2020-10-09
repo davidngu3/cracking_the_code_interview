57114dc87de63ff1a64b76cc59cc9da759740f4b
davidngu3 <d.nguyen.melb@gmail.com>
2020-10-09 18:20:16 +1100

    1.1 Is Unique with hashset and inplace

--- 1-1_is-unique.py
+++ 1-1_is-unique.py
@@ -2,16 +2,39 @@
 # cannot use additional data structures?
 
 # IDEAS:
-# If able to use additional data structures, I would use a hash table because it would allow fast lookup
+# If able to use additional data structures, I would use a hash table (dictionary in python) because it would allow fast lookup
 
 def isUnique(string): 
-    
+    alphabetHash = {}
 
+    # enter each character c into a hashtable, if already entry at c then return false
+    for char in string:
+        if char in alphabetHash:
+            return False
+        alphabetHash[char] = 1
 
-
+    return True
 
 # If not able to use additional data structures, I would first sort the string then check adjacent values for inequality
 
+def isUniqueInPlace(string):
+    sortedString = sorted(string)
+
+    for i, c in enumerate(string):
+        if i > 0: # escape first char
+            if string[i] == string[i-1]:
+                return False
+
+    return True
+
 
 if __name__ == "__main__":
-    isUnique("abra")
+    print(isUnique("bbb"))
+    print(isUnique("a"))
+    print(isUnique("abcfgh"))
+    print(isUnique(""))
+
+    print(isUniqueInPlace("bbb"))
+    print(isUniqueInPlace("a"))
+    print(isUniqueInPlace("abcfgh"))
+    print(isUniqueInPlace(""))
