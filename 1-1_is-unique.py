abd339f703bdb93feb775d06425d1e2eb350eb6e
davidngu3 <d.nguyen.melb@gmail.com>
2020-10-09 18:06:26 +1100

    first problem

---
+++ 1-1_is-unique.py
@@ -0,0 +1,17 @@
+# Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+# cannot use additional data structures?
+
+# IDEAS:
+# If able to use additional data structures, I would use a hash table because it would allow fast lookup
+
+def isUnique(string): 
+    
+
+
+
+
+# If not able to use additional data structures, I would first sort the string then check adjacent values for inequality
+
+
+if __name__ == "__main__":
+    isUnique("abra")
