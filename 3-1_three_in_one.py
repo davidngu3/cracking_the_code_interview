40e4f4802fd162bae4113f8743eced785a0d0c6a
davidngu3 <d.nguyen.melb@gmail.com>
2020-11-07 20:49:38 +1100

    stack ctci

---
+++ 3-1_three_in_one.py
@@ -0,0 +1,11 @@
+# 3.1 Three in One: Describe how you could use a single array to implement three stacks.
+
+'''
+Approach 1: Fixed division: n/3 for each stack, or based on expected stack size
+Approach 2: Flexibile divisions: Allow stacks to be flexibile in size:
+    - when one stack exceeds capacity, grow capacity and shift elements
+    - circular, such that final stack may start at end of the array and wrap around to beginning
+
+
+
+'''
