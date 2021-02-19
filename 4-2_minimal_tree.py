9d5cc77122d9dd0411aa1dfff6750334c0b9da76
davidngu3 <d.nguyen.melb@gmail.com>
2021-02-19 16:34:40 +1100

    set up BST class

--- 4-2_minimal_tree.py
+++ 4-2_minimal_tree.py
@@ -1,3 +1,62 @@
+import unittest
 # 4.2 Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algorithm
 # to create a binary search tree with minimal height.
 # Hints: #19, #73, #176
+
+class Node:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BST:
+    def __init__(self, root = None):
+        self.root = root
+
+    def insert(self, node, head = None):
+        if not self.root:  # case: no root
+            self.root = node
+
+        else:
+            if not head:
+                head = self.root
+
+            if node.val < head.val:
+                if not head.left:
+                    head.left = node
+                else:
+                    self.insert(node, head.left)
+            else:
+                if not head.right:
+                    head.right = node
+                else:
+                    self.insert(node, head.right)
+                
+            
+
+# class TestMethods(unittest.TestCase):
+#     def test_1(self):
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6]
+
+    midPoint = (len(arr) - 1) // 2 
+
+    aBST = BST()
+
+    newNode = Node(5)
+    aBST.insert(newNode)
+    newNode = Node(3)
+    aBST.insert(newNode)
+    newNode = Node(15)
+    aBST.insert(newNode)
+    newNode = Node(34)
+    aBST.insert(newNode)
+
+    print(aBST.root.val)
+    print(aBST.root.left.val)
+    print(aBST.root.right.val)
+    print(aBST.root.right.right.val)
+    
+    # unittest.main() 
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,11 @@
-node_a, node_b, node_c, node_d, node_e, node_f, node_g = setupGraph()
-    assert detectRoute(node_a, node_c) == True
-
-    node_a, node_b, node_c, node_d, node_e, node_f, node_g = setupGraph()
-    assert detectRoute(node_d, node_e) == False
-
-    node_a, node_b, node_c, node_d, node_e, node_f, node_g = setupGraph()
-    assert detectRoute(node_a, node_g) == True
-
-    node_a, node_b, node_c, node_d, node_e, node_f, node_g = setupGraph()
-    assert detectRoute(node_g, node_b) == False
+else:
+        #     if node.val < head.val:
+        #         if not head.left:
+        #             head.left = node
+        #         else:
+        #             self.insert(node, head.left)
+        #     else:
+        #         if not head.right:
+        #             head.right = node
+        #         else:
+        #             self.insert(node, head.right)
